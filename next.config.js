f467d72ea9463194ad1fc92801d24bfcbf015ff3
Vishal Lohar <www.vlohar08@gmail.com>
2023-02-08 16:33:55 +0530

    Upgraded Next.js and other packages versions

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    loader: "akamai",
-    path: "",
-  },
 };
 
 module.exports = nextConfig;
